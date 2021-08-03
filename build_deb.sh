33d529c95cc68d6b7008909777b473d8ccbc4552
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2021-08-03 14:11:31 +0200

    build_deb.sh: print list of shared dependencies

--- build_deb.sh
+++ build_deb.sh
@@ -3,4 +3,15 @@ mkdir -p target/debian && \
 markdown README.md | \
 html2text -nometa -utf8 -style pretty -rcfile .html2textrc | \
 tail -n +11 > target/debian/extended_package_description.txt && \
-cargo deb --no-strip "$@"
+cargo deb --no-strip "$@" && \
+{ rm -f target/debian/extended_package_description.txt; true; } && \
+cd target && \
+# This proper way to get dependencies was commented by H-M-H at
+# https://github.com/mmstick/cargo-deb/issues/170#issuecomment-891749827
+touch debian/control && \
+find . -iname 'packsquash' -type f -perm /ug=x | while read -r executable; do
+	printf 'Dependencies for %s: ' "$executable" && \
+	dpkg-shlibdeps "$executable" -O 2>/dev/null
+done && \
+{ rm -f debian/control; true; } && \
+cd ..
