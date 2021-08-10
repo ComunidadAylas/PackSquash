06ab7b5adf02fc64f45b2e8380d2a0f255e1946a
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2021-08-10 14:03:14 +0200

    build_deb.sh: improve readability

--- build_deb.sh
+++ build_deb.sh
@@ -1,19 +1,25 @@
-#!/bin/sh
-mkdir -p target/debian && \
+#!/bin/sh -e
+mkdir -p target/debian
+
+# Generate extended package description from README.md
 markdown README.md | \
 html2text -nometa -utf8 -style pretty -rcfile .html2textrc | \
-tail -n +11 > target/debian/extended_package_description.txt && \
-cargo deb --no-strip "$@" && \
-{ rm -f target/debian/extended_package_description.txt; true; } && \
-cd target && \
-# This proper way to get dependencies was commented by H-M-H at
+tail -n +11 > target/debian/extended_package_description.txt
+
+# Build the Debian package
+cargo deb --no-strip "$@"
+rm -f target/debian/extended_package_description.txt || true
+
+# Now print the dependencies of the package. This proper way to get
+# dependencies was commented by H-M-H at
 # https://github.com/mmstick/cargo-deb/issues/170#issuecomment-891749827
-touch debian/control && \
+cd target
+touch debian/control
 find . -iname 'packsquash' -type f -perm /ug=x | while read -r executable; do
-	printf 'Dependencies for %s: ' "$executable" && \
+	printf 'Dependencies for %s: ' "$executable"
 	dpkg-shlibdeps "$executable" -O 2>/dev/null
-done && \
-printf 'GStreamer version: ' && \
-apt-cache show gstreamer1.0-plugins-base | sed -n '/^Version:/{s/^Version: //;p}' && \
-{ rm -f debian/control; true; } && \
+done
+printf 'GStreamer version: '
+apt-cache show gstreamer1.0-plugins-base | sed -n '/^Version:/{s/^Version: //;p}'
+rm -f debian/control || true
 cd ..
