e6548f042a4c4d814b310230be952aaea99f055f
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2021-08-17 18:35:10 +0200

    Allow Clippy lint in potentially innocent code

--- src/squash_zip.rs
+++ src/squash_zip.rs
@@ -275,6 +275,17 @@ impl<F: AsyncRead + AsyncSeek + Unpin> SquashZip<F> {
 			previous_zip_contents = AHashMap::with_capacity(cdh_entry_count_hint);
 
 			// Keep adding files to the map until there are no more central directory headers
+
+			// Allow blocks in this while condition for several reasons:
+			// - We care about the side effects that evaluating the condition each time does,
+			//   so we can't just store its result in a variable.
+			// - Async closures are unstable, and using them does not really simplify things
+			//   much, because we would then have to care about how the closure borrows its
+			//   environment.
+			// - Any of the above alternatives also result in more lines of code that are not
+			//   particularly more elegant.
+			// Related Clippy issue: https://github.com/rust-lang/rust-clippy/issues/7580
+			#[allow(clippy::blocks_in_if_conditions)]
 			while {
 				previous_zip.read_exact(&mut buffer[..4]).await?;
 				buffer[..4] == [0x50, 0x4B, 0x01, 0x02]
