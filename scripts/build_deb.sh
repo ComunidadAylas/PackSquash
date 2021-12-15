4e93a0c17d848b856ec7de05c4c61ab33d9a0376
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2021-12-15 17:36:39 +0100

    Debian package CI: say goodbye to ugly hacks to get dependencies
    I won't miss them!
    
    Recent versions of cargo-deb already resolve dependencies using
    dpkg-shlibdeps. Now that we build PackSquash in a Debian Bullseye
    container, we can use the results of this command directly, as stable
    Debian releases ship the oldest package versions we care to support.
    
    Related upstream PR: https://github.com/mmstick/cargo-deb/pull/197

--- scripts/build_deb.sh
+++ scripts/build_deb.sh
@@ -9,16 +9,3 @@ tail -n +11 > target/debian/extended_package_description.txt
 # Build the Debian package
 cargo deb --no-strip "$@"
 rm -f target/debian/extended_package_description.txt || true
-
-# Now print the dependencies of the package. This proper way to get
-# dependencies was commented by H-M-H at
-# https://github.com/mmstick/cargo-deb/issues/170#issuecomment-891749827
-cd target
-touch debian/control
-find . -iname 'packsquash' -type f -perm /ug=x | while read -r executable; do
-	printf 'Dependencies for %s: ' "$executable"
-	dpkg-shlibdeps "$executable" -O 2>/dev/null
-done
-printf 'GStreamer version: '
-apt-cache show gstreamer1.0-plugins-base | sed -n '/^Version:/{s/^Version: //;p}'
-rm -f debian/control || true
