1f1eda4a72b50e96a5dc142d744d91f3c67ede0e
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2021-12-18 19:18:04 +0100

    png_file: add tests for quirks introduced in 64e339c059e8de0b945e095009b2c1955f622c0c
    These tests help ensuring that those workarounds have the expected
    effects.

--- src/pack_file/png_file/tests.rs
+++ src/pack_file/png_file/tests.rs
@@ -8,6 +8,7 @@ use crate::config::ColorQuantizationTarget;
 use super::*;
 
 static PNG_DATA: &[u8] = include_bytes!("example.png");
+static ENDERMAN_EYES_DATA: &[u8] = include_bytes!("enderman_eyes.png");
 
 /// Processes the given input data as a [PngFile], using the provided settings,
 /// expecting a successful result.
@@ -16,13 +17,15 @@ async fn successful_process_test(
 	settings: PngFileOptions,
 	expect_same_pixels: bool,
 	expect_smaller_file_size: bool,
+	expect_same_color_type: bool,
+	asset_type: PackFileAssetType,
 	test_name: &str
 ) {
 	let input_data_len = input_data.len();
 
 	let data_stream = PngFile {
 		read: Builder::new().read(input_data).build(),
-		asset_type: PackFileAssetType::GenericTexture,
+		asset_type,
 		file_length_hint: input_data_len,
 		optimization_settings: settings
 	}
@@ -40,6 +43,7 @@ async fn successful_process_test(
 
 	let mut decoded_pixels;
 	let processed_data_size;
+	let processed_color_type;
 	{
 		let mut data = Vec::with_capacity(input_data_len);
 		for (_, partial_data) in process_result {
@@ -56,6 +60,9 @@ async fn successful_process_test(
 			.with_output_format(Format::Rgba8)
 			.read_info()
 			.expect("No error should happen while decoding processed PNG");
+
+		processed_color_type = image_info.color_type;
+
 		decoded_pixels = vec![0; image_info.buffer_size];
 		png_reader
 			.next_frame(&mut decoded_pixels)
@@ -64,12 +71,16 @@ async fn successful_process_test(
 	let decoded_pixels = decoded_pixels.as_rgba();
 
 	let mut original_pixels;
+	let original_color_type;
 	{
 		let (image_info, mut png_reader) = spng::Decoder::new(input_data)
 			.with_decode_flags(DecodeFlags::GAMMA | DecodeFlags::TRANSPARENCY)
 			.with_output_format(Format::Rgba8)
 			.read_info()
 			.expect("No error should happen while decoding example PNG");
+
+		original_color_type = image_info.color_type;
+
 		original_pixels = vec![0; image_info.buffer_size];
 		png_reader
 			.next_frame(&mut original_pixels)
@@ -94,6 +105,11 @@ async fn successful_process_test(
 		!expect_smaller_file_size || processed_data_size < input_data_len,
 		"The processed PNG should be smaller than the original PNG"
 	);
+
+	assert!(
+		!expect_same_color_type || original_color_type == processed_color_type,
+		"The processed PNG should have the same color type as the original PNG"
+	);
 }
 
 #[tokio::test]
@@ -102,10 +118,13 @@ async fn lossless_optimization_works() {
 		PNG_DATA,
 		PngFileOptions {
 			color_quantization_target: ColorQuantizationTarget::None,
+			skip_alpha_optimizations: true,
 			..Default::default()
 		},
-		true, // Same pixels
-		true, // Smaller size
+		true,  // Same pixels
+		true,  // Smaller size
+		false, // Not necessarily the same color type
+		PackFileAssetType::GenericTexture,
 		"lossless_optimization_works"
 	)
 	.await
@@ -121,7 +140,45 @@ async fn lossy_optimization_works() {
 		},
 		false, // Not necessarily the same pixels
 		true,  // Smaller size
+		false, // Not necessarily the same color type
+		PackFileAssetType::GenericTexture,
 		"lossy_optimization_works"
+	)
+	.await
+}
+
+#[tokio::test]
+async fn entity_eye_blending_workaround_works() {
+	successful_process_test(
+		ENDERMAN_EYES_DATA,
+		PngFileOptions {
+			color_quantization_target: ColorQuantizationTarget::EightBitDepth,
+			do_not_change_transparent_pixel_colors: true,
+			..Default::default()
+		},
+		true,  // Same pixels
+		false, // Not necessarily smaller
+		false, // Not necessarily the same color type
+		PackFileAssetType::EyeLayer,
+		"entity_eye_blending_workaround_works"
+	)
+	.await
+}
+
+#[tokio::test]
+async fn banner_layer_check_workaround_works() {
+	successful_process_test(
+		PNG_DATA,
+		PngFileOptions {
+			color_quantization_target: ColorQuantizationTarget::EightBitDepth,
+			skip_color_type_reduction: true,
+			..Default::default()
+		},
+		false, // Not necessarily the same pixels
+		false, // Not necessarily smaller
+		true,  // Same color type
+		PackFileAssetType::BannerLayer,
+		"banner_layer_check_workaround_works"
 	)
 	.await
 }
