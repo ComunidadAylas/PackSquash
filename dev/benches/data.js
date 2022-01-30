0c985a574bdf75ca12a62f7d649526c9e991cb69
github-action-benchmark <github@users.noreply.github.com>
2022-01-30 19:03:35 +0000

    add PackSquash library quick benchmarks (cargo) benchmark result for cb67cf04fbe0a1fa4a04ac5e06a024e4683d7b7b

---
+++ dev/benches/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1643569415659,
+  "repoUrl": "https://github.com/ComunidadAylas/PackSquash",
+  "entries": {
+    "PackSquash library quick benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "AlexTMjugador@users.noreply.github.com",
+            "name": "AlexTMjugador",
+            "username": "AlexTMjugador"
+          },
+          "committer": {
+            "email": "AlexTMjugador@users.noreply.github.com",
+            "name": "AlexTMjugador",
+            "username": "AlexTMjugador"
+          },
+          "distinct": true,
+          "id": "cb67cf04fbe0a1fa4a04ac5e06a024e4683d7b7b",
+          "message": "bench: minor tweaks to make it behave nicer",
+          "timestamp": "2022-01-30T19:36:35+01:00",
+          "tree_id": "e2c5b16db24169235601048fcb6b71b1583a09de",
+          "url": "https://github.com/ComunidadAylas/PackSquash/commit/cb67cf04fbe0a1fa4a04ac5e06a024e4683d7b7b"
+        },
+        "date": 1643569414584,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tiny_benches_wall_time/empty_pack",
+            "value": 15636473,
+            "range": "± 540268",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/aylas_khron_micro_pack",
+            "value": 1347088048,
+            "range": "± 18596201",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/jilchu_chronos_micro_pack",
+            "value": 8948381333,
+            "range": "± 79685396",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
