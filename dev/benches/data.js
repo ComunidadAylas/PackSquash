aeb3a6c634f8e3f641a1c4e8c0413bc3415e450f
github-action-benchmark <github@users.noreply.github.com>
2022-01-31 01:10:15 +0000

    add PackSquash library quick benchmarks (cargo) benchmark result for cb67cf04fbe0a1fa4a04ac5e06a024e4683d7b7b

--- dev/benches/data.js
+++ dev/benches/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1643569415659,
+  "lastUpdate": 1643591415229,
   "repoUrl": "https://github.com/ComunidadAylas/PackSquash",
   "entries": {
     "PackSquash library quick benchmarks": [
@@ -41,6 +41,46 @@ window.BENCHMARK_DATA = {
             "name": "small_benches_wall_time/jilchu_chronos_micro_pack",
             "value": 8948381333,
             "range": "± 79685396",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "AlexTMjugador",
+            "username": "AlexTMjugador",
+            "email": "AlexTMjugador@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "AlexTMjugador",
+            "username": "AlexTMjugador",
+            "email": "AlexTMjugador@users.noreply.github.com"
+          },
+          "id": "cb67cf04fbe0a1fa4a04ac5e06a024e4683d7b7b",
+          "message": "bench: minor tweaks to make it behave nicer",
+          "timestamp": "2022-01-30T18:36:35Z",
+          "url": "https://github.com/ComunidadAylas/PackSquash/commit/cb67cf04fbe0a1fa4a04ac5e06a024e4683d7b7b"
+        },
+        "date": 1643591414224,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tiny_benches_wall_time/empty_pack",
+            "value": 15095728,
+            "range": "± 847463",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/aylas_khron_micro_pack",
+            "value": 1299990795,
+            "range": "± 10756511",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/jilchu_chronos_micro_pack",
+            "value": 8498065538,
+            "range": "± 78846578",
             "unit": "ns/iter"
           }
         ]
