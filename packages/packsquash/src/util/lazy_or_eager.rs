57b9896288a3adeb25ab05e54da88dc19532bf5c
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2022-10-09 19:49:05 +0200

    fix(blockstate): handle non-vanilla assets better. Optimize things

---
+++ packages/packsquash/src/util/lazy_or_eager.rs
@@ -0,0 +1,18 @@
+use once_cell::sync::Lazy;
+use std::ops::Deref;
+
+pub enum LazyOrEager<'lazy, T, F: FnOnce() -> T> {
+	Lazy(&'lazy Lazy<T, F>),
+	Eager(T)
+}
+
+pub impl<T, F: FnOnce() -> T> Deref for LazyOrEager<'_, T, F> {
+	type Target = T;
+
+	fn deref(&self) -> &Self::Target {
+		match self {
+			Self::Lazy(lazy) => lazy,
+			Self::Eager(inner) => inner
+		}
+	}
+}
