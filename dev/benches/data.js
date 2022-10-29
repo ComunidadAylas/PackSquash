bb82138a606844d9257ca72329ddc18c9cf6d9a0
github-action-benchmark <github@users.noreply.github.com>
2022-10-29 16:21:00 +0000

    add PackSquash library quick benchmarks (cargo) benchmark result for 0d19b2b58a15c5486f2122b0dc28f59d06d05408

--- dev/benches/data.js
+++ dev/benches/data.js
@@ -1,56 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1666801945132,
+  "lastUpdate": 1667060460297,
   "repoUrl": "https://github.com/ComunidadAylas/PackSquash",
   "entries": {
     "PackSquash library quick benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "AlexTMjugador@users.noreply.github.com",
-            "name": "Alejandro González",
-            "username": "AlexTMjugador"
-          },
-          "committer": {
-            "email": "AlexTMjugador@users.noreply.github.com",
-            "name": "Alejandro González",
-            "username": "AlexTMjugador"
-          },
-          "distinct": true,
-          "id": "269ae867ff6f17501150d1993479c632b1d580c1",
-          "message": "chore(deps): update OxiPNG dependency\n\nSome patches of ours were merged upstream, and a new release has been\npublished. Our patched fork was updated accordingly.",
-          "timestamp": "2022-09-05T20:29:49Z",
-          "tree_id": "865dcd335d66401e204a34c8080c1f51fcec504b",
-          "url": "https://github.com/ComunidadAylas/PackSquash/commit/269ae867ff6f17501150d1993479c632b1d580c1"
-        },
-        "date": 1662412586261,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "tiny_benches_wall_time/empty_pack",
-            "value": 14388062,
-            "range": "± 761664",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/aylas_khron_micro_pack",
-            "value": 471945660,
-            "range": "± 18444951",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/jilchu_chronos_micro_pack",
-            "value": 12694125927,
-            "range": "± 121765988",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
-            "value": 3155993568,
-            "range": "± 194571653",
-            "unit": "ns/iter"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2399,6 +2351,54 @@ window.BENCHMARK_DATA = {
             "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
             "value": 2522164642,
             "range": "± 108706004",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "0d19b2b58a15c5486f2122b0dc28f59d06d05408",
+          "message": "fix(deps): update rust crate patricia_tree to 0.4.0",
+          "timestamp": "2022-10-29T13:38:50Z",
+          "tree_id": "69b5ea762ff05d835ef1ba38c5b808e2f5e683b4",
+          "url": "https://github.com/ComunidadAylas/PackSquash/commit/0d19b2b58a15c5486f2122b0dc28f59d06d05408"
+        },
+        "date": 1667060459353,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tiny_benches_wall_time/empty_pack",
+            "value": 10511553,
+            "range": "± 266940",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/aylas_khron_micro_pack",
+            "value": 359600765,
+            "range": "± 8088110",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/jilchu_chronos_micro_pack",
+            "value": 14800643921,
+            "range": "± 79973823",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
+            "value": 2248416524,
+            "range": "± 87372765",
             "unit": "ns/iter"
           }
         ]
