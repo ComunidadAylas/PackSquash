027f3d409d6c4ceeb35f9f38f7578a7197ba2ecb
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2022-11-14 14:25:30 +0100

    fix(gui): drop unnecesary and troublesome min-h-height at ViewContainer
    This caused an obvious vertical misalignment issue on Windows WebView2,
    based on Edge, which in turn is based on Chromium. However, dropping
    that CSS declaration didn't cause any issue I could notice on WebView2,
    Linux WebKitGTK and Firefox.

--- packages/packsquash_gui/src/components/ViewContainer.tsx
+++ packages/packsquash_gui/src/components/ViewContainer.tsx
@@ -1,17 +1,11 @@
 import { ComponentProps, splitProps } from "solid-js";
 
-export default (
-  props: ComponentProps<"div"> & { fitContentHeight?: boolean }
-) => {
-  const [componentProps, divProps] = splitProps(props, [
-    "class",
-    "fitContentHeight"
-  ]);
+export default (props: ComponentProps<"div">) => {
+  const [, divProps] = splitProps(props, ["class"]);
 
   return (
     <div
       class="absolute inset-0 flex flex-col gap-2 overflow-auto p-2 sm:p-4"
-      classList={{ "min-h-fit": componentProps.fitContentHeight }}
       {...divProps}
     >
       {divProps.children}
