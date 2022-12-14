4b3ce5f8777cae327084cff80d2f92bdf17996e6
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2022-12-14 11:23:13 +0100

    fix(util/strip_utf8_bom): always consume as many bytes as read to buf
    Draining an ArrayVec preemptively empties the original slice even if no
    data is read to the buffer, which generates incorrect output that can
    only be detected under certain conditions.
    
    To fix that, do not drain the ArrayVec, and remove elements from its
    head instead. This is not a particularly efficient operation to do, but
    given the extremely small sizes we're dealing with here it doesn't
    matter in practice.

--- packages/packsquash/src/util/strip_utf8_bom.rs
+++ packages/packsquash/src/util/strip_utf8_bom.rs
@@ -1,8 +1,5 @@
-/// Returns a new [`Read`] that skips the initial byte order mark character
-/// that some programs (most notably, Microsoft software, like Notepad) add
-/// to UTF-8 encoded files. If no BOM is present, the provided [`Read`] is
-/// returned as-is.
-// TODO docs
+//! Contains the implementation of the [`StripUtf8BomExt`] trait.
+
 use std::convert::Infallible;
 use std::error::Error;
 use std::io::{self, Read};
@@ -14,7 +11,18 @@ const BOM_UTF8: [u8; 3] = [0xEF, 0xBB, 0xBF];
 /// character, in bytes.
 const BOM_UTF8_SIZE: usize = BOM_UTF8.len();
 
+/// Extension trait for byte sources that provides a method to ergonomically
+/// strip an optional initial byte order mark character.
+///
+/// Byte order marks (BOMs) are not required or recommended for UTF-8, but
+/// some programs, notably older Microsoft Notepad versions, store and expect
+/// them.
 pub trait StripUtf8BomExt<T: Read, E: Error> {
+	/// Strips the initial UTF-8 BOM character from the bytes yielded by this
+	/// reader. If there is no initial UTF-8 BOM character, no bytes are stripped.
+	///
+	/// Implementations of this method are guaranteed to be efficient, avoiding
+	/// buffering the reader data as much as possible.
 	fn strip_utf8_bom(self) -> Result<T, E>;
 }
 
@@ -52,6 +60,9 @@ impl<'slice> StripUtf8BomExt<&'slice [u8], Infallible> for &'slice [u8] {
 	}
 }
 
+/// An adapter that either passes through operations to an inner reader
+/// or yields non-BOM header bytes until exhaustion and chains reads to
+/// an inner reader.
 pub enum StripUtf8Bom<R: Read> {
 	RemovedBom(R),
 	RewindHeader {
@@ -69,8 +80,12 @@ impl<R: Read> Read for StripUtf8Bom<R> {
 				rest
 			} => {
 				let mut bytes_read = 0;
-				for (buffer_byte, header_byte) in buf.iter_mut().zip(non_bom_header.drain(..)) {
-					*buffer_byte = header_byte;
+				let mut buf_iter = buf.iter_mut();
+
+				while let (Some(buffer_byte), false) = (buf_iter.next(), non_bom_header.is_empty()) {
+					// non_bom_header is at most BOM_UTF8_SIZE elements long, so this O(n) remove is fast
+					// (draining does rotate_left once and requires a comparison, which is not better for small arrays)
+					*buffer_byte = non_bom_header.remove(0);
 					bytes_read += 1;
 				}
 
@@ -110,6 +125,23 @@ mod tests {
 	}
 
 	#[test]
+	fn strip_utf8_with_small_read_buf_works() {
+		let read = Cursor::new([0xAA, 0xBB]);
+		let mut read = read.strip_utf8_bom().expect("Unexpected I/O failure");
+		let mut buf = [0];
+
+		read.read_exact(&mut buf).expect("Unexpected I/O failure");
+		assert_eq!(buf[0], 0xAA);
+
+		read.read_exact(&mut buf).expect("Unexpected I/O failure");
+		assert_eq!(buf[0], 0xBB);
+
+		assert!(
+			matches!(read.read_exact(&mut buf), Err(err) if err.kind() == io::ErrorKind::UnexpectedEof)
+		);
+	}
+
+	#[test]
 	fn slice_strip_utf8_bom_specialization_works() {
 		strip_utf8_bom_and_expect::<&[u8], _>(&[0xEF, 0xBB, 0xBF, 0xAA, 0xBB][..], &[0xAA, 0xBB]);
 	}
@@ -122,5 +154,13 @@ mod tests {
 	#[test]
 	fn small_data_is_not_stripped() {
 		strip_utf8_bom_and_expect(Cursor::new([0xAA, 0xBB]), &[0xAA, 0xBB]);
+	}
+
+	#[test]
+	fn big_data_without_utf8_bom_is_not_stripped() {
+		strip_utf8_bom_and_expect::<&[u8], _>(
+			&[0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF][..],
+			&[0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF]
+		);
 	}
 }
