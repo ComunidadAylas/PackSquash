382910b8c434dea89eaca30151c013ccabcf6e1f
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2023-03-15 20:51:36 +0100

    chore: rename CLI executable back to "packsquash"
    CLI users care about saving keystrokes, and this is more compatible.

--- packages/packsquash_cli/build.rs
+++ packages/packsquash_cli/build.rs
@@ -6,17 +6,14 @@ fn main() {
 
 #[cfg(windows)]
 fn add_executable_metadata() {
-	let mut windows_resource = winresource::WindowsResource::new();
-	windows_resource.set("LegalCopyright", env!("CARGO_PKG_AUTHORS"));
-	windows_resource.set(
-		"FileDescription",
-		&format!("PackSquash - {}", env!("CARGO_PKG_DESCRIPTION"))
-	);
-
-	windows_resource.set_language(0x0409); // English (US)
-	windows_resource.set_icon("../../icons/packsquash_icon_256x256.ico");
-
-	windows_resource
+	inresource::WindowsResource::new()
+		.set("LegalCopyright", env!("CARGO_PKG_AUTHORS"))
+		.set(
+			"FileDescription",
+			&format!("PackSquash - {}", env!("CARGO_PKG_DESCRIPTION"))
+		)
+		.set_language(0x0409) // English (US)
+		.set_icon("../../icons/packsquash_icon_256x256.ico")
 		.compile()
 		.expect("Windows executable resource build failure");
 }
