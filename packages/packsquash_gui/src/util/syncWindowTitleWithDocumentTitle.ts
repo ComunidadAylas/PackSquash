886f544135224dc9bb7cf54c212aefc60d49c08c
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2023-03-17 17:48:54 +0100

    fix(gui/frontend): make document title sync with window title properly
    The l10n context may asynchronously change the <title> element text
    nodes once translated text is available, but a MutationObserver that is
    subscribed to only DOM structure changes may not get triggered when this
    happens. This is the case sometimes on Linux WebKit, at least.
    
    To fix that, also observe character data mutations.

--- packages/packsquash_gui/src/util/syncWindowTitleWithDocumentTitle.ts
+++ packages/packsquash_gui/src/util/syncWindowTitleWithDocumentTitle.ts
@@ -7,15 +7,16 @@ export default () => {
     const currentTitle = document.title;
 
     // Comparing title strings is cheaper than inspecting the actual mutations
-    if (currentTitle !== previousTitle) {
+    if (currentTitle != "" && currentTitle !== previousTitle) {
       appWindow.setTitle(currentTitle).catch();
       previousTitle = currentTitle;
     }
   }).observe(
     // The SolidJS MetaProvider title change works by recreating the <title> element.
     // Thus, listening for changes to a <title> element won't work. Instead, we have
-    // to listen for changes in <head> children
+    // to listen for changes in <head> children. In addition, l10n may change already
+    // existing title character data once the key gets translated
     document.head,
-    { subtree: true, childList: true }
+    { subtree: true, childList: true, characterData: true }
   );
 };
