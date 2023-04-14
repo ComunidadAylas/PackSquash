ComunidadAylas/PackSquash

commit b6de89409f5b9f548ccedc87b7612abcccdfe93e

chore: rename CLI executable back to "packsquash"
CLI users care about saving keystrokes, and this is more compatible.

packages/packsquash_cli/build.rs
@@ -6,17 +6,14 @@ fn main() {
 
 #[cfg(windows)]
 fn add_executable_metadata() {
-	let mut windows_resource = winres::WindowsResource::new();
-	windows_resource.set("LegalCopyright", env!("CARGO_PKG_AUTHORS"));
-	windows_resource.set(
-		"FileDescription",
-		&format!("PackSquash - {}", env!("CARGO_PKG_DESCRIPTION"))
-	);
-
-	windows_resource.set_language(0x0409); // English (US)
-	windows_resource.set_icon("../../icons/packsquash_icon_256x256.ico");
-
-	windows_resource
+	inresource::WindowsResource::new()
+		.set("LegalCopyright", env!("CARGO_PKG_AUTHORS"))
+		.set(
+			"FileDescription",
+			&format!("PackSquash - {}", env!("CARGO_PKG_DESCRIPTION"))
+		)
+		.set_language(0x0409) // English (US)
+		.set_icon("../../icons/packsquash_icon_256x256.ico")
 		.compile()
 		.expect("Windows executable resource build failure");
 }

.github/workflows/ci.yml
@@ -109,7 +109,7 @@ jobs:
             rust-toolchain-targets: x86_64-pc-windows-gnu
             tauri-target: x86_64-pc-windows-gnu
             runner: windows-latest
-            cli-executable: packsquash_cli.exe
+            cli-executable: packsquash.exe
 
           - target: aarch64-unknown-linux-gnu
             host-target: x86_64-unknown-linux-gnu
@@ -125,13 +125,13 @@ jobs:
             rust-toolchain-targets: aarch64-apple-darwin
             tauri-target: universal-apple-darwin
             runner: macos-latest
-            cli-executable: packsquash_cli
+            cli-executable: packsquash
 
           - target: aarch64-apple-darwin
             host-target: x86_64-apple-darwin
             rust-toolchain-targets: aarch64-apple-darwin
             runner: macos-latest
-            cli-executable: packsquash_cli
+            cli-executable: packsquash
 
     runs-on: ${{ matrix.runner }}
 
@@ -374,13 +374,13 @@ jobs:
           path: packsquash-aarch64
 
       - name: 🔨 Generate universal CLI binary
-        run: lipo -create -output packsquash_cli packsquash-x64/packsquash_cli packsquash-aarch64/packsquash_cli
+        run: lipo -create -output packsquash packsquash-x64/packsquash packsquash-aarch64/packsquash
 
       - name: 📤 Upload universal CLI binary
         uses: actions/upload-artifact@v3
         with:
           name: PackSquash CLI executable (universal-apple-darwin)
-          path: packsquash_cli
+          path: packsquash
 
   build-docker-images:
     name: Build Docker images

deny.toml
@@ -121,7 +121,6 @@ skip-tree = [
 unknown-registry = "deny"
 unknown-git = "deny"
 allow-git = [
-	"https://github.com/Nilstrieb/winres",
 	"https://github.com/tauri-apps/tauri-plugin-store"
 ]
 

packages/packsquash_cli/Cargo.toml
@@ -29,9 +29,11 @@ winapi = { version = "0.3.9", default-features = false, features = ["consoleapi"
 winapi-util = "0.1.5"
 
 [target.'cfg(windows)'.build-dependencies]
-# Fix winres not working on the latest Rust toolchain.
-# Related PR: https://github.com/mxre/winres/pull/41
-winres = { version = "0.1.11", git = "https://github.com/Nilstrieb/winres", rev = "c839134b" }
+winresource = "0.1.15"
+
+[[bin]]
+name = "packsquash"
+bench = false
 
 [package.metadata.deb]
 name = "packsquash"
@@ -41,7 +43,7 @@ license-file = ["../../LICENSE", "0"]
 extended-description-file = "target/debian/extended_package_description.txt"
 suggests = "packsquash-gui"
 assets = [
-	["target/release/packsquash_cli", "usr/bin/packsquash", "755"],
+	["target/release/packsquash", "usr/bin/packsquash", "755"],
 	["../../README.md", "usr/share/doc/packsquash/README.md", "644"]
 ]
 section = "utils"
@@ -49,7 +51,7 @@ section = "utils"
 # increment this, and change APT repo action
 revision = "0"
 
-[package.metadata.winres]
+[package.metadata.winresource]
 ProductName = "PackSquash"
 CompanyName = "Comunidad Aylas"
 OriginalFilename = "packsquash.exe"

commit 8852ba7f012ab4220c6217722e71596c6a7b836d

chore: fix typo introduced in b6de89409f5b9f548ccedc87b7612abcccdfe93e

packages/packsquash_cli/build.rs
@@ -6,7 +6,7 @@ fn main() {
 
 #[cfg(windows)]
 fn add_executable_metadata() {
-	inresource::WindowsResource::new()
+	winresource::WindowsResource::new()
 		.set("LegalCopyright", env!("CARGO_PKG_AUTHORS"))
 		.set(
 			"FileDescription",