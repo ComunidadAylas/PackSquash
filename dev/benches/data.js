4112d46f04b9a15c2f8057c7d02c1fe052b04bdd
github-action-benchmark <github@users.noreply.github.com>
2023-05-21 10:13:42 +0000

    add PackSquash library quick benchmarks (cargo) benchmark result for 428cc044867903d47c7fbc30d1178b805ca13b9c

--- dev/benches/data.js
+++ dev/benches/data.js
@@ -1,56 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1684513257387,
+  "lastUpdate": 1684664022808,
   "repoUrl": "https://github.com/ComunidadAylas/PackSquash",
   "entries": {
     "PackSquash library quick benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "29139614+renovate[bot]@users.noreply.github.com",
-            "name": "renovate[bot]",
-            "username": "renovate[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "fba324393233aad2f2fab698a3e6371e08754609",
-          "message": "fix(deps): update rust crate sysinfo to 0.28.4",
-          "timestamp": "2023-03-24T20:10:51Z",
-          "tree_id": "a43b8522382e6174038c0c62652c396cd61a747b",
-          "url": "https://github.com/ComunidadAylas/PackSquash/commit/fba324393233aad2f2fab698a3e6371e08754609"
-        },
-        "date": 1679707369781,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "tiny_benches_wall_time/empty_pack",
-            "value": 10686498,
-            "range": "± 10122316",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/aylas_khron_micro_pack",
-            "value": 343148535,
-            "range": "± 3143383",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/jilchu_chronos_micro_pack",
-            "value": 5312131116,
-            "range": "± 21037158",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
-            "value": 664315982,
-            "range": "± 11455834",
-            "unit": "ns/iter"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2399,6 +2351,54 @@ window.BENCHMARK_DATA = {
             "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
             "value": 274330556,
             "range": "± 4597762",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "428cc044867903d47c7fbc30d1178b805ca13b9c",
+          "message": "fix(deps): update rust crate java-properties to v2 (#222)\n\n* fix(deps): update rust crate java-properties to v2\r\n\r\n* chore(clippy): fix new lints\r\n\r\n---------\r\n\r\nCo-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>\r\nCo-authored-by: Alejandro González <me@alegon.dev>",
+          "timestamp": "2023-05-21T11:38:00+02:00",
+          "tree_id": "5183cfeb080c6d2419a0dc2011935d295adb6d3b",
+          "url": "https://github.com/ComunidadAylas/PackSquash/commit/428cc044867903d47c7fbc30d1178b805ca13b9c"
+        },
+        "date": 1684664021604,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tiny_benches_wall_time/empty_pack",
+            "value": 13529112,
+            "range": "± 12868829",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/aylas_khron_micro_pack",
+            "value": 195156520,
+            "range": "± 3173213",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/jilchu_chronos_micro_pack",
+            "value": 2241027611,
+            "range": "± 78577416",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
+            "value": 309825118,
+            "range": "± 5220158",
             "unit": "ns/iter"
           }
         ]
