ec4bd7e467d692b037e29499c9d839780947543d
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2023-05-22 20:31:51 +0200

    perf(model_processor): ignore builtin models for caching purposes
    This leads to a more efficient and correct cache usage even if the pack
    contains files that would not be read by the game.

--- packages/packsquash/src/pack_processor/java/asset_processor/item_and_block_model_asset_processor/cache.rs
+++ packages/packsquash/src/pack_processor/java/asset_processor/item_and_block_model_asset_processor/cache.rs
@@ -7,6 +7,7 @@ use packsquash_options::GlobalOptions;
 use quick_cache::sync::Cache;
 
 use super::{
+	is_builtin_model_path,
 	item_or_block_model::{ItemOrBlockModel, ItemOrBlockModelRepresentative},
 	InnerItemOrBlockModelAssetError
 };
@@ -52,6 +53,13 @@ impl<'params, 'state, V: VirtualFileSystem + ?Sized, F: Read + Seek + Send>
 		&self,
 		asset_path: &RelativePath<'static>
 	) -> Result<Option<Arc<RwLock<CachedItemOrBlockModel>>>, InnerItemOrBlockModelAssetError> {
+		// Built-in models should not appear in resource packs, but if they do the game
+		// ignores their version on the pack. Therefore, also do that ourselves to avoid
+		// cache pollution
+		if is_builtin_model_path(asset_path) {
+			return Ok(None);
+		}
+
 		Ok(self.cache.get_or_insert_with(asset_path, || {
 			json_helper::deserialize::<ItemOrBlockModelRepresentative, _, _, _>(
 				asset_path,
