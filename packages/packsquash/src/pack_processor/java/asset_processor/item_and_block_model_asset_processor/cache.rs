1869c3c40b33733eb2b840402cc94de0c505b4e3
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2023-05-22 22:33:04 +0200

    perf(model_processor): fine-tune model cache size
    Let's also add documentation explaining how we got to that value and
    other interesting facts about the whole purpose of caching.

--- packages/packsquash/src/pack_processor/java/asset_processor/item_and_block_model_asset_processor/cache.rs
+++ packages/packsquash/src/pack_processor/java/asset_processor/item_and_block_model_asset_processor/cache.rs
@@ -18,8 +18,25 @@ use crate::{
 	vfs::VirtualFileSystem
 };
 
-// TODO gather stats to dynamically size this cache better
-const CACHE_SIZE: usize = 128;
+/// The maximum number of deserialized models that will be stored in the memory cache.
+/// Caching 64 models on the 1.19.2 vanilla resource pack with
+/// `process_not_self_referenced_and_non_vanilla_assets = false` gives a hit ratio of ~22.5% (the
+/// theoretical maximum would be 60.38%), which provides a ~4% speedup on a certain test
+/// environment. Caching more models (e.g., 128) doubles memory usage for a meager ~2% increase
+/// in hit ratio and negligible speedups, while caching less models (e.g., 32) decreases the hit
+/// ratio ~2% but increases runtime a ~1.3%, so 64 should be a good sweet spot for realistic usage.
+///
+/// We fix the cache size because that provides for simpler eviction algorithms and overall more
+/// efficient cache operation. The downside is that the cache won't take into account the memory
+/// usage of models when eviction is necessary, so memory usage might be a bit higher than if it
+/// did.
+///
+/// Observe that, on systems with enough available RAM and proper OS filesystem caches, this cache
+/// only helps reducing syscall and deserialization costs. Its impact is greater when deserialization
+/// costs are high (e.g., complex models) and if the OS filesystem cache is not caching models. For
+/// the aforementioned pack, a 64 entry cache takes ~1 MiB of RAM (measured with `accounting-allocator`),
+/// which is within noise margin nowadays.
+const CACHE_SIZE: usize = 64;
 
 pub(super) struct ModelCache<'params, 'state, V: VirtualFileSystem + ?Sized, F: Read + Seek + Send> {
 	cache: Cache<RelativePath<'static>, Option<Arc<RwLock<CachedItemOrBlockModel>>>>,
