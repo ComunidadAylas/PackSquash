93d93ad4414dbdb33a26d12626e6ce48546124ab
Alejandro González <me@alegon.dev>
2023-07-20 18:35:48 +0200

    ci(gui): fix not being able to locate GUI binary

--- packages/packsquash_gui/wdio.conf.ts
+++ packages/packsquash_gui/wdio.conf.ts
@@ -16,7 +16,7 @@ export const config: Options.Testrunner = {
       browserName: "wry",
       // @ts-expect-error "tauri:options" is a Tauri-specific WebDriver proxy extension capability
       "tauri:options": {
-        application: "../../target/release/packsquash_gui"
+        application: `../../target/${process.env.CARGO_BUILD_TARGET ? `${process.env.CARGO_BUILD_TARGET}/` : ""}release/packsquash_gui`
       }
     }
   ],
