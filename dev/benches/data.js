f6fd698eedadb29f3f6f26908999013bfd3baa1b
github-action-benchmark <github@users.noreply.github.com>
2023-08-21 14:01:19 +0000

    add PackSquash library quick benchmarks (cargo) benchmark result for 29003831b69247fab7427961c215cc2c9dab0e75

--- dev/benches/data.js
+++ dev/benches/data.js
@@ -1,56 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692616846490,
+  "lastUpdate": 1692626479749,
   "repoUrl": "https://github.com/ComunidadAylas/PackSquash",
   "entries": {
     "PackSquash library quick benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "29139614+renovate[bot]@users.noreply.github.com",
-            "name": "renovate[bot]",
-            "username": "renovate[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "263fd6a70b30d24f462ae628dcab3af33caa1008",
-          "message": "fix(deps): update rust crate rlimit to 0.10.1",
-          "timestamp": "2023-07-19T19:41:36Z",
-          "tree_id": "80d0305621a2579bbc6020563aff135a2de60a65",
-          "url": "https://github.com/ComunidadAylas/PackSquash/commit/263fd6a70b30d24f462ae628dcab3af33caa1008"
-        },
-        "date": 1689805065561,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "tiny_benches_wall_time/empty_pack",
-            "value": 9390043,
-            "range": "± 224445",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/aylas_khron_micro_pack",
-            "value": 210588059,
-            "range": "± 1339985",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/jilchu_chronos_micro_pack",
-            "value": 2078942956,
-            "range": "± 13868214",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
-            "value": 286387552,
-            "range": "± 2527713",
-            "unit": "ns/iter"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2399,6 +2351,54 @@ window.BENCHMARK_DATA = {
             "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
             "value": 85589491,
             "range": "± 1112840",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@alegon.dev",
+            "name": "Alejandro González",
+            "username": "AlexTMjugador"
+          },
+          "committer": {
+            "email": "me@alegon.dev",
+            "name": "Alejandro González",
+            "username": "AlexTMjugador"
+          },
+          "distinct": true,
+          "id": "29003831b69247fab7427961c215cc2c9dab0e75",
+          "message": "chore: migrate from `time` to `chrono`\n\n`chrono` is now actively maintained, and `time` has been taking some\nmandatory political decisions I, quite frankly, do not agree with, and\ncause me significant burnout to (not) deal with every few months.",
+          "timestamp": "2023-08-21T15:18:31+02:00",
+          "tree_id": "51f79584fc58365968beddfcf47e3b895efbf7fb",
+          "url": "https://github.com/ComunidadAylas/PackSquash/commit/29003831b69247fab7427961c215cc2c9dab0e75"
+        },
+        "date": 1692626478329,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tiny_benches_wall_time/empty_pack",
+            "value": 14079067,
+            "range": "± 680372",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/aylas_khron_micro_pack",
+            "value": 211690128,
+            "range": "± 6868987",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/jilchu_chronos_micro_pack",
+            "value": 3351766725,
+            "range": "± 49170822",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
+            "value": 340202246,
+            "range": "± 8097697",
             "unit": "ns/iter"
           }
         ]
