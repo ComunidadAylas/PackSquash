800c392f2fe3279ff44f5700b2f20b3cf4f4ebd1
github-action-benchmark <github@users.noreply.github.com>
2023-12-10 07:01:14 +0000

    add PackSquash library quick benchmarks (cargo) benchmark result for 930c44673c4f7061db415a4d434cefd2fd8616de

--- dev/benches/data.js
+++ dev/benches/data.js
@@ -1,56 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702085824424,
+  "lastUpdate": 1702191674641,
   "repoUrl": "https://github.com/ComunidadAylas/PackSquash",
   "entries": {
     "PackSquash library quick benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "29139614+renovate[bot]@users.noreply.github.com",
-            "name": "renovate[bot]",
-            "username": "renovate[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "a2528fb356dad30fbaa12afec5bce49fb2660a0d",
-          "message": "fix(deps): update rust crate entropy to 0.4.2",
-          "timestamp": "2023-10-18T16:08:40Z",
-          "tree_id": "a9809887caf24ce44a8d0ae4f3049347c91f85dd",
-          "url": "https://github.com/ComunidadAylas/PackSquash/commit/a2528fb356dad30fbaa12afec5bce49fb2660a0d"
-        },
-        "date": 1697655661018,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "tiny_benches_wall_time/empty_pack",
-            "value": 8325797,
-            "range": "± 328754",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/aylas_khron_micro_pack",
-            "value": 164221242,
-            "range": "± 5130110",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/jilchu_chronos_micro_pack",
-            "value": 4317447431,
-            "range": "± 78625495",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
-            "value": 1086746033,
-            "range": "± 11813316",
-            "unit": "ns/iter"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2399,6 +2351,54 @@ window.BENCHMARK_DATA = {
             "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
             "value": 735397600,
             "range": "± 1260232",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "930c44673c4f7061db415a4d434cefd2fd8616de",
+          "message": "chore(deps): update taiki-e/install-action digest to 6ee6c3a",
+          "timestamp": "2023-12-10T04:27:57Z",
+          "tree_id": "9f57f0a6724d173fc49d1d3a937ee24906b25f29",
+          "url": "https://github.com/ComunidadAylas/PackSquash/commit/930c44673c4f7061db415a4d434cefd2fd8616de"
+        },
+        "date": 1702191673443,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tiny_benches_wall_time/empty_pack",
+            "value": 10480863,
+            "range": "± 1860698",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/aylas_khron_micro_pack",
+            "value": 140811000,
+            "range": "± 1507348",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/jilchu_chronos_micro_pack",
+            "value": 2681970192,
+            "range": "± 80946377",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
+            "value": 738739140,
+            "range": "± 2146548",
             "unit": "ns/iter"
           }
         ]
