95def25e57c7352e4c13d52d156e482d7cb2aecd
github-action-benchmark <github@users.noreply.github.com>
2024-08-04 03:46:20 +0000

    add PackSquash library quick benchmarks (cargo) benchmark result for 17c443316ad149d87483a03ca98297ca57d04302

--- dev/benches/data.js
+++ dev/benches/data.js
@@ -1,56 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722704148604,
+  "lastUpdate": 1722743180357,
   "repoUrl": "https://github.com/ComunidadAylas/PackSquash",
   "entries": {
     "PackSquash library quick benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "29139614+renovate[bot]@users.noreply.github.com",
-            "name": "renovate[bot]",
-            "username": "renovate[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "a08e059be5f9e5de5fec58302073392c735f5e4c",
-          "message": "chore(deps): update taiki-e/install-action digest to c2c0b6e",
-          "timestamp": "2024-06-25T02:20:15Z",
-          "tree_id": "a090f0675d1909aebc1e153a5b68487be0f3c2eb",
-          "url": "https://github.com/ComunidadAylas/PackSquash/commit/a08e059be5f9e5de5fec58302073392c735f5e4c"
-        },
-        "date": 1719291471196,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "tiny_benches_wall_time/empty_pack",
-            "value": 9699674,
-            "range": "± 1578394",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/aylas_khron_micro_pack",
-            "value": 143078055,
-            "range": "± 1692568",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/jilchu_chronos_micro_pack",
-            "value": 2751946667,
-            "range": "± 21318539",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
-            "value": 610708795,
-            "range": "± 3347530",
-            "unit": "ns/iter"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2399,6 +2351,54 @@ window.BENCHMARK_DATA = {
             "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
             "value": 674316271,
             "range": "± 3707236",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "17c443316ad149d87483a03ca98297ca57d04302",
+          "message": "chore(deps): update ci dependencies (#312)\n\nCo-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>",
+          "timestamp": "2024-08-04T03:23:59Z",
+          "tree_id": "0f1941b80c54c67a4f3ffb48987ec4683b207d7d",
+          "url": "https://github.com/ComunidadAylas/PackSquash/commit/17c443316ad149d87483a03ca98297ca57d04302"
+        },
+        "date": 1722743179830,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tiny_benches_wall_time/empty_pack",
+            "value": 9551880,
+            "range": "± 68810",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/aylas_khron_micro_pack",
+            "value": 129895272,
+            "range": "± 3872889",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/jilchu_chronos_micro_pack",
+            "value": 2717196721,
+            "range": "± 117393327",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
+            "value": 667794434,
+            "range": "± 2990866",
             "unit": "ns/iter"
           }
         ]
