692a424c88c59bfdcc5a44ebf1fb96ead3cab0d8
github-action-benchmark <github@users.noreply.github.com>
2024-08-25 19:28:05 +0000

    add PackSquash library quick benchmarks (cargo) benchmark result for 49d3c20fe33f13c69f03f2a243bcbfa13e6a44d2

--- dev/benches/data.js
+++ dev/benches/data.js
@@ -1,56 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724558030245,
+  "lastUpdate": 1724614085684,
   "repoUrl": "https://github.com/ComunidadAylas/PackSquash",
   "entries": {
     "PackSquash library quick benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "me@alegon.dev",
-            "name": "Alejandro González",
-            "username": "AlexTMjugador"
-          },
-          "committer": {
-            "email": "me@alegon.dev",
-            "name": "Alejandro González",
-            "username": "AlexTMjugador"
-          },
-          "distinct": true,
-          "id": "332caed6d8df7bfc2594cc579affb04e5da31d30",
-          "message": "ci: ensure external merge queue PRs get their checks in place",
-          "timestamp": "2024-07-29T11:37:49+02:00",
-          "tree_id": "43335d04c6fe90374a1ba5a4cd045e3f0f769dde",
-          "url": "https://github.com/ComunidadAylas/PackSquash/commit/332caed6d8df7bfc2594cc579affb04e5da31d30"
-        },
-        "date": 1722247300116,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "tiny_benches_wall_time/empty_pack",
-            "value": 9470302,
-            "range": "± 1621537",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/aylas_khron_micro_pack",
-            "value": 118669615,
-            "range": "± 3406477",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/jilchu_chronos_micro_pack",
-            "value": 2727897169,
-            "range": "± 67572389",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
-            "value": 667259039,
-            "range": "± 3045551",
-            "unit": "ns/iter"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2399,6 +2351,54 @@ window.BENCHMARK_DATA = {
             "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
             "value": 656591499,
             "range": "± 4905176",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "49d3c20fe33f13c69f03f2a243bcbfa13e6a44d2",
+          "message": "chore(deps): update dependency idna to v3.8",
+          "timestamp": "2024-08-25T17:33:17Z",
+          "tree_id": "fd8ad6caecfedcdcdc120af27336f757ffd45387",
+          "url": "https://github.com/ComunidadAylas/PackSquash/commit/49d3c20fe33f13c69f03f2a243bcbfa13e6a44d2"
+        },
+        "date": 1724614084950,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tiny_benches_wall_time/empty_pack",
+            "value": 9485930,
+            "range": "± 171311",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/aylas_khron_micro_pack",
+            "value": 120645844,
+            "range": "± 3131978",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/jilchu_chronos_micro_pack",
+            "value": 2560904697,
+            "range": "± 33577484",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
+            "value": 656707195,
+            "range": "± 4016604",
             "unit": "ns/iter"
           }
         ]
