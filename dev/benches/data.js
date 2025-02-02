c4774b8a4c238d2deec4e2507feabc14f04ba8d2
github-action-benchmark <github@users.noreply.github.com>
2025-02-02 05:24:13 +0000

    add PackSquash library quick benchmarks (cargo) benchmark result for 51b60be28dd4517db4d49c9f7d5da845eb67ef5d

--- dev/benches/data.js
+++ dev/benches/data.js
@@ -1,56 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738451041415,
+  "lastUpdate": 1738473853850,
   "repoUrl": "https://github.com/ComunidadAylas/PackSquash",
   "entries": {
     "PackSquash library quick benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "29139614+renovate[bot]@users.noreply.github.com",
-            "name": "renovate[bot]",
-            "username": "renovate[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "d0ccf8a7bd6e28cd88c1138700f584cca2047461",
-          "message": "chore(deps): update taiki-e/install-action digest to 6aa8b42",
-          "timestamp": "2024-12-04T23:26:18Z",
-          "tree_id": "09072e8353a31db5113c1a58acf806c6047f07dd",
-          "url": "https://github.com/ComunidadAylas/PackSquash/commit/d0ccf8a7bd6e28cd88c1138700f584cca2047461"
-        },
-        "date": 1733366128505,
-        "tool": "cargo",
-        "benches": [
-          {
-            "name": "tiny_benches_wall_time/empty_pack",
-            "value": 9905639,
-            "range": "± 159201",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/aylas_khron_micro_pack",
-            "value": 123215720,
-            "range": "± 3934191",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/jilchu_chronos_micro_pack",
-            "value": 2693366715,
-            "range": "± 73163090",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
-            "value": 670117551,
-            "range": "± 3694652",
-            "unit": "ns/iter"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2399,6 +2351,54 @@ window.BENCHMARK_DATA = {
             "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
             "value": 605605007,
             "range": "± 1785671",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "51b60be28dd4517db4d49c9f7d5da845eb67ef5d",
+          "message": "chore(deps): update taiki-e/install-action digest to 0dfccb3",
+          "timestamp": "2025-02-02T01:31:35Z",
+          "tree_id": "198db8f655b5f65791e9e087034fefea3bb85751",
+          "url": "https://github.com/ComunidadAylas/PackSquash/commit/51b60be28dd4517db4d49c9f7d5da845eb67ef5d"
+        },
+        "date": 1738473853210,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tiny_benches_wall_time/empty_pack",
+            "value": 9902090,
+            "range": "± 914947",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/aylas_khron_micro_pack",
+            "value": 117900774,
+            "range": "± 4241087",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/jilchu_chronos_micro_pack",
+            "value": 2931560205,
+            "range": "± 24966028",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "small_benches_wall_time/aiamded_breadstick_micro_pack",
+            "value": 605444650,
+            "range": "± 1988225",
             "unit": "ns/iter"
           }
         ]
